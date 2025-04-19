51fd567daa69365bf2d648122e8e69b11ac63398
johnnyboi12 <54293281+JohnSmoit@users.noreply.github.com>
2025-04-19 12:28:58 -0700

    remove faulty plugin configuration

--- after/plugin/clangd-cc.lua
+++
@@ -1,1 +0,0 @@
-require("clangd-cc").setup({})
--- lua/johnsmoit/plugins/clangd-cc.lua
+++
@@ -1,7 +0,0 @@
-return {
-    dir = "~/Develop/nvim/plugin/clangd-cc",
-    name = "clangd-cc",
-    config = function()
-        require("clangd-cc")
-    end
-}
