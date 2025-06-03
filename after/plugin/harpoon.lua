4c330aff8e93ce93782bf02bd72614373589bbc9
JohnSmoit <ju.smoit@gmail.com>
2025-06-02 23:20:21 -0700

    Remove annoying ass file picking keybind that I set for some reason

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,10 +8,6 @@ require('telescope').load_extension('harpoon')
 -- nice keybindings for basic harpoon actions
 vim.keymap.set('n', '<leader>mf', mark.add_file)
 vim.keymap.set('n', '<leader>mm', ui.toggle_quick_menu)
-vim.keymap.set('n', '<C-n>', function()
-	local fileNum = tonumber(vim.fn.input('Mark #: '))
-	ui.nav_file(fileNum)
-end)
 
-vim.keymap.set('n', '<C-Right>', ui.nav_next)
-vim.keymap.set('n', '<C-Left>', ui.nav_prev)
+vim.keymap.set('n', '<C-B>n', ui.nav_next)
+vim.keymap.set('n', '<C-B>l', ui.nav_prev)
--- lua/johnsmoit/commands.lua
+++ lua/johnsmoit/commands.lua
@@ -1,5 +1,15 @@
 vim.api.nvim_create_user_command('Conf', function()
-    vim.cmd.Oil(vim.fn.stdpath("config"))
+    local configPath = vim.fn.stdpath("config")
+    vim.cmd.Oil(configPath)
+    vim.fn.chdir(configPath)
     vim.cmd('echo "The madness continues..."')
 end, {})
 
+
+-- WIP: Set based on current buffer directory options as well as oil viewed directory
+vim.api.nvim_create_user_command("SetDir", function(opts)
+    vim.notify(string.format("SetDir args: %s", vim.inspect(opts.fargs)), vim.log.levels.INFO)
+end, {
+        desc = "Changes the working directory to the provided directory (or the directory of the active buffer if none is specified)",
+        nargs = "?"
+    })
