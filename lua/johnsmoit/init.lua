936617eaebc73fa3877f86b236ff4a7ed74856d0
JohnSmoit <ju.smoit@gmail.com>
2025-06-03 18:22:40 -0700

    LSPless LETS GO BABY!!!!

--- lua/johnsmoit/init.lua
+++ lua/johnsmoit/init.lua
@@ -9,6 +9,7 @@ jconf.commands = require('johnsmoit.commands')
 jconf.autoClose = require('johnsmoit.autoClose')
 jconf.windowing = require('johnsmoit.windowing')
 jconf.format_utils = require("johnsmoit.format_utils")
+jconf.lsp = require("johnsmoit.lumpy_space_princess")
 
 require("johnsmoit.setup")
 
